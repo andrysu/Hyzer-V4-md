a064bd57ee73f73f4eaee8b02cd70d55e0958214
andrysu <97227953+andrysu@users.noreply.github.com>
2022-04-04 15:56:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const yargs = require('yargs/yargs')
 const rl = Readline.createInterface(process.stdin, process.stdout)
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
-CFonts.say('BOT BY\n YUTA', {
+CFonts.say('BOT BY\n 𝘼𝙣𝙙𝙧𝙮𝙬𝞪', {
   colors: ['blueBright','yellowBright'],                                        font: 'block',
   align: 'center',
 })
-CFonts.say(`BOT WHATSAPP BY YUTA`, {                                            
+CFonts.say(`BOT WHATSAPP BY 𝘼𝙣𝙙𝙧𝙮𝙬𝞪`, {                                            
 colors: ['yellow'],                                                           
 font: 'console',                                                              align: 'center',
 })
